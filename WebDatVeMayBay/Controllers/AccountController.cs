aedd6dc7addc5149a796d3adf72a179e6d1c9059
PhucNguy3n <phucnguyen201913@gmail.com>
2024-11-14 12:17:37 +0700

    FormTimCB-Login-SignUp-PWReset

---
+++ WebDatVeMayBay/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDatVeMayBay.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            return View();
+        }
+        public ActionResult PassReset()
+        {
+            return View();
+        }
+    }
+}
