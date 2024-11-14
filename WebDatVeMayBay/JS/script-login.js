f91a60078db5cf7f8bef8651f0772ea81e229434
lmtthao <thaolove7a13@gmail.com>
2024-11-14 18:10:43 +0700

    update login page

---
+++ WebDatVeMayBay/JS/script-login.js
@@ -0,0 +1,19 @@
+﻿const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
+
+//Show password
+const passwordInput = document.getElementById('password'); // assuming you have an element with id="password"
+const showPasswordCheckbox = document.getElementById('showPassword');
+
+showPasswordCheckbox.addEventListener('change', () => {
+    passwordInput.type = showPasswordCheckbox.checked ? 'text' : 'password';
+});
