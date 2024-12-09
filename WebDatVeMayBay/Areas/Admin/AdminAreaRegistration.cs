bfea06b843645adc8382ae9c7fe8fc7d6fb48a8b
lmtthao <thaolove7a13@gmail.com>
2024-12-09 11:36:02 +0700

    bo sung css dang ky dang nhap

--- WebDatVeMayBay/Areas/Admin/AdminAreaRegistration.cs
+++ WebDatVeMayBay/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace WebDatVeMayBay.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                new[] { "WebDatVeMayBay.Areas.Admin.Controllers" }
             );
         }
     }
